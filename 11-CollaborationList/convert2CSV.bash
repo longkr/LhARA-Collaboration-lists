90438e109530f71ef1f6464e54bac92953e495f9
Kenneth Long <k.long@imperial.ac.uk>
2024-10-17 17:14:33 +0100

    Mild refactor

---
+++ 11-CollaborationList/convert2CSV.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+in2csv Belfast.xlsx > Belfast.csv
+in2csv Birmingham.xlsx > Birmingham.csv
+in2csv CERN.xlsx > CERN.csv
+in2csv CNRS-Institut-Curie.xlsx > CNRS-Institut-Curie.csv
+in2csv Corerain.xlsx > Corerain.csv
+in2csv DOE-LBNL.xlsx > DOE-LBNL.csv
+in2csv INFN-CT.xlsx > INFN-CT.csv
+in2csv Imperial.xlsx > Imperial.csv
+in2csv InstituteCancerResearch.xlsx > InstituteCancerResearch.csv
+in2csv Lancaster.xlsx > Lancaster.csv
+in2csv LeoCancerCare.xlsx > LeoCancerCare.csv
+in2csv Liverpool.xlsx > Liverpool.csv
+in2csv Manchester.xlsx > Manchester.csv
+in2csv Maxeler.xlsx > Maxeler.csv
+in2csv Oxford.xlsx > Oxford.csv
+in2csv QueenMary.xlsx > QueenMary.csv
+in2csv RHUL.xlsx > RHUL.csv
+in2csv RosalindFranklinInstitut.xlsx > RosalindFranklinInstitut.csv
+in2csv STFC.xlsx > STFC.csv
+in2csv Strathclyde.xlsx > Strathclyde.csv
+in2csv Surrey.xlsx > Surrey.csv
+in2csv Swansea.xlsx > Swansea.csv
+in2csv UCL.xlsx > UCL.csv
